eac178936ba6db7a8392ecbc677320e2470a97c5
unknown <kenzieli23@gmail.com>
2022-03-04 17:29:59 -0600

    changed  to ->conn

--- config/Database.php
+++ config/Database.php
@@ -9,9 +9,9 @@
     $database = ltrim($dbparts['path'], '/');
 
     try{
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         //set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
         }
     catch(PDOException $e)

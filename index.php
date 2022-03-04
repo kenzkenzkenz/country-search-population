8d13ab7463e321bcbe7fb7ca843460fd31ba6ba7
unknown <kenzieli23@gmail.com>
2022-03-03 22:02:01 -0600

    trying again

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php 
+    $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+
+    include_once "config/Database.php"; 
+
+    if ($country) {
+        $query = 'SELECT * FROM country WHERE Name LIKE :country ORDER BY Population DESC';
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':country', "%" . $country . "%");
+        $statement->execute();
+        //$statement->debugDumpParams();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $statement->closeCursor();
+    }
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,5 +28,22 @@
     <header>
         <h1>Get a Country's Population</h1>
     </header>
+    <section>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
+            <h2 id="enter-a-country">Enter a Country:</h2>
+            <input type="text" id="country" name="country" aria-labelledby="enter-a-country" required>
+            <button>Submit</button>
+        </form>
+    </section>
+    <section>
+    <?php if (!empty($results)) { ?>
+        <h2><?php echo count($results) ?> Results</h2>
+        <?php foreach ($results as $result) {
+            echo "<p>{$result['Name']} - Pop: {$result['Population']} - Code: {$result['Code']}</p>";
+        } ?>
+    <?php } else {
+        echo "<p>No Results.</p>";
+    } ?>
+    </section>
 </body>
 </html>

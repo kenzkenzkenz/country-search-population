344e923795cc9984aab571af28c292890d55925d
unknown <kenzieli23@gmail.com>
2022-03-05 18:39:43 -0600

    al

--- config/Database.php
+++ config/Database.php
@@ -5,19 +5,14 @@
 
     $hostname = $dbparts['host'];
     $username = $dbparts['user'];
-    $password = getenv('PASSWORD');
+    $password = $dbparts['pass'];
+    //$password = getenv('PASSWORD');
     $database = ltrim($dbparts['path'], '/');
 
     try{
-        $this->conn = new PDO(
-        'mysql:host=' . $this->host . 
-        ';dbname=' . $this->db_name, 
-        $this->username, 
-        $this->password
-    );
-        //$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
         //set the PDO error mode to exception
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
         }
     catch(PDOException $e)

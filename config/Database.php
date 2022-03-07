3203a9308ae16f80923af64e824f222d55521565
unknown <kenzieli23@gmail.com>
2022-03-06 19:20:25 -0600

    back to semi working

--- config/Database.php
+++ config/Database.php
@@ -4,17 +4,17 @@
 
     $hostname = $dbparts['host'];
     $username = $dbparts['user'];
-    //$password = $dbparts['pass'];
-    $password = getenv('PASSWORD');
+    $password = $dbparts['pass'];
+    //$password = getenv('PASSWORD');
     $database = ltrim($dbparts['path'], '/');
 
     try{
-        //$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
 
         //set the PDO error mode to exception
-        //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully";
         }
     catch(PDOException $e)

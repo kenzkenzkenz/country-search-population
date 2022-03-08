b9b714013212ac181d0494a180d419ef88fef863
unknown <kenzieli23@gmail.com>
2022-03-07 20:28:16 -0600

    basic

--- config/Database.php
+++ config/Database.php
@@ -1,15 +1,20 @@
 <?php
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
+    // $url = getenv('JAWSDB_URL');
+    // $dbparts = parse_url($url);
 
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    //$password = getenv('PASSWORD');
-    $database = ltrim($dbparts['path'], '/');
+    // $hostname = $dbparts['host'];
+    // $username = $dbparts['user'];
+    // $password = $dbparts['pass'];
+    // //$password = getenv('PASSWORD');
+    // $database = ltrim($dbparts['path'], '/');
+
+    $hostname = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $username = 'h5bl2txltjkl6hzh';
+    $password = 'w8a2y7ktf3nczo8d';
+    $database = 'pw5d2fj3gsgx5v4g';
 
     try{
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $conn = new PDO("mysql:host={$hostname};dbname={$database}", $username, $password);
         //$this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
 
         //set the PDO error mode to exception

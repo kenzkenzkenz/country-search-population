b7625d421951a26d7a642797c08d9ce59f22c233
unknown <kenzieli23@gmail.com>
2022-03-07 20:48:36 -0600

    dbparts

--- config/Database.php
+++ config/Database.php
@@ -1,17 +1,17 @@
 <?php
-    // $url = getenv('JAWSDB_URL');
-    // $dbparts = parse_url($url);
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
 
-    // $hostname = $dbparts['host'];
-    // $username = $dbparts['user'];
-    // $password = $dbparts['pass'];
-    // //$password = getenv('PASSWORD');
-    // $database = ltrim($dbparts['path'], '/');
+    $host = $dbparts['host'];
+    $db_name = ltrim($dbparts['path'], '/');
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    //$password = getenv('PASSWORD');
 
-    $host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    $db_name = 'pw5d2fj3gsgx5v4g';
-    $username = 'h5bl2txltjkl6hzh';
-    $password = 'w8a2y7ktf3nczo8d';
+    //$host = 'eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    //$db_name = 'pw5d2fj3gsgx5v4g';
+    //$username = 'h5bl2txltjkl6hzh';
+    //$password = 'w8a2y7ktf3nczo8d';
 
     try {
         $db = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
